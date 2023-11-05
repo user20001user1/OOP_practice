084fd6e9f5657e9fbb5b5baee86daaf5568abd26
User <kovalenkoaleksej844@gmail.com>
2023-11-05 02:06:26 +0200

    done practical work #4.

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,11 +1,35 @@
 ﻿#include <iostream>
+#include <Windows.h>
 #include "Apartment.h"
 #include "HotelRoom.h"
 #include "ApartmentBase.h"
+#include "MyDerivedClass.h"
 using namespace std;
+
+void createObjectsAndCallMethod() {
+    MyBaseClass* objects[5];
+    cout << endl<< "Create 5 objects:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << i+1 << ". Оберіть клас для створення об'єкта (1 або 2): ";
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            objects[i] = new MyDerivedClass1;
+        }
+        else if (choice == 2) {
+            objects[i] = new MyDerivedClass2;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        objects[i]->myVirtualMethod();
+    }
+}
 
 int main()
 {
+    SetConsoleCP(1251); 
+    SetConsoleOutputCP(1251);
 	/*Apartment object1, object2, object3;
 	std::cin >> object1 >> object2 >> object3;
 
@@ -18,4 +42,7 @@ int main()
 
 	ApartmentBase b;
 	b.Print();
+
+    createObjectsAndCallMethod();
 }
+
